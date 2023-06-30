8ca396fe16e971ec5c53d6259f0868db1e73440d
mar10fam <mphamrtin@gmail.com>
2023-06-29 18:42:16 -0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost"
+
 function App() {
   return (
-    <div className="App">
-      <h1>React Starter</h1>
-    </div>
+    <Router>
+      <Routes>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/createpost"> Post </Link>
+          <Link to="/login"> Login </Link>
+
+        </nav>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
