cfe68f4235cd663f2b580db6981bb55b98f04377
mar10fam <mphamrtin@gmail.com>
2023-08-31 16:52:12 -0700

    navbar and authentification

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import {React, useState } from 'react';
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
+import { useNavigate } from "react";
 
 function CreatePost() {
+  const [title, setTitle] = useState("")
+  const [postText, setPostText] = useState("")
+  const postCollectionRef = collection(db, "posts");
+
+  let navigate = useNavigate();
+  const createPost = async () => {
+    await addDoc(postCollectionRef, { 
+      title, 
+      postText, 
+      author: {
+        name: auth.currentUser.displayName, id: auth.currentUser.uid
+      }
+    });
+    navigate("/");
+  };
+
   return (
-    <div> CreatePost </div>
+    <div className="createPostPage"> 
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp">
+          <label> Title:</label>
+          <input placeholder="Title..." 
+          onChange={(event) => {
+            setTitle(event.target.value);
+            }} />
+        </div>
+        <div className="inputGp">
+          <label> Post:</label>
+          <textarea placeholder="Post..." 
+          onChange={(event) => {
+            setPostText(event.target.value);
+          }} />
+        </div>
+        <button onClick={createPost}>Submit Post</button>
+      </div> 
+    </div>
   )
 }
 
